ba3eff1d3a44ee863bec6f05baf89de44d221fd9
opilane <opilane@HP-CZC0359NP1>
2024-05-09 12:13:27 +0300

    Add project files.

---
+++ PrivateProtectedStudent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class PrivateProtectedStudent
+    {
+        private protected string PrivateProtectedStudent1 =
+            "PrivateProtectedStudent1";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using ClassLibrarylol;
+
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kapseldamine");
+
+            Student student = new Student();
+
+            student.Id = 101;
+            student.Name = "Test";
+            student.Email = "arvikadak@gmail.com";
+
+            Console.WriteLine("Id = " + student.Id);
+            Console.WriteLine("Name = " + student.Name);
+            Console.WriteLine("Email = " + student.Email);
+
+
+            ProtectedStudent protectedStudent = new ProtectedStudent();
+            //protectedStudent.DoSomething();
+            Console.WriteLine("---------------------------------");
+            Program program = new Program();
+            program.DoSometingInProgramClass();
+
+            Console.WriteLine("-----------Private Protected----------");.
+            Program pp = new Program();
+            Console.WriteLine(pp.privateProtectedInProgramClass =
+                "privateProtectedInProgramClassMethod");
+
+            var privateProtectedStudent = new PrivateProtectedStudent();
+            //privateProtectedStudent.PrivateProtectedStudent1 = "asdasdasd";
+            Console.WriteLine("--------Sealed Class--------");
+            var sc = new SealedStudent();
+            sc.Id = 123;
+            sc.Name = "Test";
+            sc.Email = "arvikadak@gmail.com";
+            Console.WriteLine($"Id on {sc.Id}, Name on {sc.Name} ja Email on {sc.Email}" );
+
+
+        }
+
+        protected void DoSometingInProgramClass()
+        {
+            Console.WriteLine("DoSomethingInProgramClass");
+        }
+
+        private protected string privateProtectedInProgramClass =
+            "privateProtectedInProgramClass";
+    }
+}
